5880ab38ce4f4155114fb679acc2d53966d8b3ed
Sooraj Gupta <soorajgupta00@gmail.com>
2024-02-01 03:15:40 +0530

    previous one was incomplete this is the complete one

--- server.js
+++ server.js
@@ -3,4 +3,8 @@ const port = 5000;
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
 app.listen(port, () => console.log(`Server is listening on Port ${port}`));
