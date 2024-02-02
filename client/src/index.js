b90dfdc7e74c1acfe84f242c76c172cf8d612aeb
Sooraj Gupta <soorajgupta00@gmail.com>
2024-02-03 00:49:55 +0530

    added dark theme

--- client/src/index.js
+++ client/src/index.js
@@ -3,3 +3,13 @@ import Modal from "./components/Modal";
 import "./css/style.css";
 
 const modal = new Modal();
+const darkModeButton = document.querySelector("#darkModeButton");
+
+darkModeButton.addEventListener("click", () => {
+  document.body.classList.toggle("light");
+  if (darkModeButton.classList.contains("fa-sun")) {
+    darkModeButton.className = "fa-solid fa-moon";
+  } else {
+    darkModeButton.className = "fa-solid fa-sun";
+  }
+});
